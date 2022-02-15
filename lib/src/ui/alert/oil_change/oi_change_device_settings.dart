a6d0ddedaf963f99a2894ec94d92d9e4fee53965
mevos19 <evolusimo@gmail.com>
2022-02-15 12:21:46 +0100

    oil change alert + towing alert

---
+++ lib/src/ui/alert/oil_change/oi_change_device_settings.dart
@@ -0,0 +1,40 @@
+// To parse this JSON data, do
+//
+//     final settingPerDevice = settingPerDeviceFromJson(jsonString);
+
+import 'dart:convert';
+
+SettingPerDevice settingPerDeviceFromJson(String str) =>
+    SettingPerDevice.fromJson(json.decode(str));
+
+String settingPerDeviceToJson(SettingPerDevice data) =>
+    json.encode(data.toJson());
+
+class SettingPerDevice {
+  SettingPerDevice({
+    required this.isActive,
+    required this.nextOilChange,
+    required this.alertBefor,
+    required this.deviceId,
+  });
+
+  final bool isActive;
+  final double nextOilChange;
+  final double alertBefor;
+  final String deviceId;
+
+  factory SettingPerDevice.fromJson(Map<String, dynamic> json) =>
+      SettingPerDevice(
+        isActive: json["is_active"],
+        nextOilChange: json["next_oil_change"].toDouble(),
+        alertBefor: json["alert_befor"].toDouble(),
+        deviceId: json["device_id"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "is_active": isActive,
+        "next_oil_change": nextOilChange,
+        "alert_befor": alertBefor,
+        "device_id": deviceId,
+      };
+}
